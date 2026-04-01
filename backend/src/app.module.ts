aa278462556180c5226e274310e41201efd46a45
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2026-04-01 20:22:41 -0300

    Importado mongo no módulo principal

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,18 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
 import { WhatsappClientModule } from './whatsapp-client/whatsapp-client.module';
 import { BotMessageBufferService } from './bot/bot-message-buffer.service';
 import { WhatsappBotService } from './bot/whatsapp-bot.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const { MONGODB_HOST, MONGODB_PORT, MONGODB_DATABASE } = process.env;
+
+const mongoUrl = `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`;
 
 @Module({
-  imports: [EventEmitterModule.forRoot(), WhatsappClientModule],
+  imports: [
+    EventEmitterModule.forRoot(),
+    WhatsappClientModule,
+    MongooseModule.forRoot(mongoUrl),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
