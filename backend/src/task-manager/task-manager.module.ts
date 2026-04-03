71e1d978f75568300f178b6ce72907dd8a467442
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2026-04-03 18:01:46 -0300

    Ajustado TaskManagerModule para importar corretamente o módulo do Mongoose

--- backend/src/task-manager/task-manager.module.ts
+++ backend/src/task-manager/task-manager.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Task, TaskSchema } from './task/task.schema';
 
 @Module({
-  imports: [EventEmitterModule.forRoot()],
+  imports: [
+    EventEmitterModule.forRoot(),
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+  ],
 })
 export class TaskManagerModule {}
