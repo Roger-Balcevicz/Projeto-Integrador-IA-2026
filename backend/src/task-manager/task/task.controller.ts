43db43c7c6a6e570aad1715b022bf6cb175b60d4
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2026-04-05 21:55:07 -0300

    Anotados métodos de TaskController com UseGuards, para impedir que sejam acessados sem autenticação

--- backend/src/task-manager/task/task.controller.ts
+++ backend/src/task-manager/task/task.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { Task } from './task.schema';
 import { TaskService } from './task.service';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
 
 @Controller('tasks')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async listAll(): Promise<Task[]> {
     return this.taskService.findAll();
   }
 
   @Get('/:id')
+  @UseGuards(JwtAuthGuard)
   async get(@Param() id: string): Promise<Task | null> {
     return this.taskService.findById(id);
   }
